854393c158e1ae3d3278bc98f7b113107ef2f68f
ted19b <ted19berenger@gmail.com>
2020-05-03 15:13:26 +0200

    remove quotes in script

--- labely-frontend/deploy.sh
+++ labely-frontend/deploy.sh
@@ -5,7 +5,7 @@ image_name=labely-app
 echo INFO : Step 1 --- building docker image...
 echo name of docker image : [ $image_name ]
 
-echo "Build docker image from Dockerfile"
+echo Build docker image from Dockerfile
 docker build -t $image_name .
 
 # Stage 2 : Start a container with name license-app-container
@@ -19,12 +19,12 @@ docker container stop $container_name
 
 # 2. remove stopped container
 echo INFO : remove stopped container [ $container_name ]
-docker rm "$container_name"
+docker rm $container_name
 
 # 3. Start a new conatiner with specified name
 echo INFO : Create new container with name [ $container_name ]
 docker run --name $container_name -d -p 4500:80 $image_name
 
 # 4. go to http://localhost:4500 to see the app
-echo "INFO : show the url to see app on the browser"
+echo INFO : show the url to see app on the browser
 echo Please go to http://localhost:4500/ to see the app on the browser
